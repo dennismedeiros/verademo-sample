9cc184f7ff6247deb7fc847a1498717a8b753720
Dennis Medeiros <me@dennismedeiros.com>
2022-07-18 09:55:16 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace verademo_sample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
